5e3b6faa4eb12ed1d5947a88389951e1fcadcfa0
nahedelfekih <elfekihnahed@gmail.com>
2022-09-26 10:41:00 +0100

    dom project

---
+++ src/Data.js
@@ -0,0 +1,30 @@
+let shopItemsData = [
+  {
+    id: "jfhgbvnscs",
+    name: "Soin Anti Imperfections",
+    price: 13,
+    desc: "Soin anti-imperfections à l'acide salycilique.",
+    img: "images/103697.jpg",
+  },
+  {
+    id: "ioytrhndcv",
+    name: "Hyalu B5 Sérum",
+    price: 35,
+    desc: "Sérum à l'acide hyaluronique anti-rides peau terne et fatiguée.",
+    img: "images/29wm_farmabank.jpeg",
+  },
+  {
+    id: "wuefbncxbsn",
+    name: "écran Anti Imperfections",
+    price: 20,
+    desc: "très haute protection solaire  enrichie en Eau Thermale.",
+    img: "images/anthelios-anti-imperfections-gel-creme-correcteur-spf50-50ml.jpg",
+  },
+  {
+    id: "thyfhcbcv",
+    name: "Gel Moussant Purifiant",
+    price: 14,
+    desc: "nettoyant peau grasse à tendance acnéique Purifie la peau.",
+    img: "images/la-roche-posay-effaclar-gel-moussant-purifiant-effaclar-13-1625216575.png",
+  },
+];
